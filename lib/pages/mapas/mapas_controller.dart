ca5ff56a699e88cf9b6bdd4d416ccfbfb63fed45
Roger Luis <rogerordaz98@gmail.com>
2022-04-09 13:00:51 -0400

    los ultimos cambios

--- lib/pages/mapas/mapas_controller.dart
+++ lib/pages/mapas/mapas_controller.dart
@@ -7,10 +7,13 @@ import 'package:get/state_manager.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:marker_icon/marker_icon.dart';
+import 'package:location/location.dart';
 
 class MapasController extends GetxController {
   var markerlist = <Marker>[].obs;
   Rx<Position?> position = Rx<Position?>(null);
+  Rx<StreamSubscription<Position>?> positionStream =
+      Rx<StreamSubscription<Position>?>(null);
 
   @override
   void onInit() async {
@@ -44,11 +47,68 @@ class MapasController extends GetxController {
     );
   }
 
-  updateLocation() async {
-    position.value = await Geolocator.getLastKnownPosition();
+  void checkGps() async {
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isLocationServiceEnabled) {
+      bool locationGps = await Location.instance.requestService();
+    }
+  }
 
-    await addMarkerToMap(
-        LatLng(position.value!.latitude, position.value!.longitude), "1");
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  updateLocation() async {
+    try {
+      await _determinePosition();
+      position.value = await Geolocator.getLastKnownPosition();
+
+      positionStream.value = Geolocator.getPositionStream(
+        locationSettings: const LocationSettings(
+          distanceFilter: 1,
+        ),
+      ).listen((Position position) {
+        this.position.value = position;
+      });
+
+      await addMarkerToMap(
+          LatLng(position.value!.latitude, position.value!.longitude), "1");
+    } on Exception catch (e) {
+      print(e);
+    }
   }
 
   Future<BitmapDescriptor> convertIconFromAsset(String imgPath) async {
