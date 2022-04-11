ad7c168416e78c5501046b8c0ea6337a435e7f38
Roger Luis <rogerordaz98@gmail.com>
2022-04-11 13:02:32 -0400

    casi al terminar las polyine

--- lib/Cliente/pages/mapa/mapa_client_page.dart
+++ lib/Cliente/pages/mapa/mapa_client_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:ndialog/ndialog.dart';
 import 'package:urbe_taxis/Cliente/pages/mapa/widgets/mapa_client_controller.dart';
 
 import 'widgets/botton_modal_client.dart';
@@ -19,21 +20,52 @@ class MapaClientPage extends StatelessWidget {
       drawer: const CustomDrawer(),
       body: Stack(
         children: [
-          GoogleMap(
-            onCameraMove: (qwe) {
-              print(qwe.target);
-            },
-            zoomControlsEnabled: false,
-            initialCameraPosition: const CameraPosition(
-              target: LatLng(22.42259, -83.69854),
-              zoom: 16,
-            ),
-            //   markers: Set<Marker>.of(controller.markerlist.value),
-            myLocationEnabled: false,
-          ),
+          (controller.isRepintMapa.value == true)
+              ? GoogleMap(
+                  onCameraMove: (qwe) {
+                    controller.markercoords.value = qwe.target;
+                  },
+                  zoomControlsEnabled: false,
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(22.42259, -83.69854),
+                    zoom: 16,
+                  ),
+
+                  polylines: controller.polyline!.toSet(),
+                  // markers: Set<Marker>.of(controller.markerlist.value),
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                )
+              : GoogleMap(
+                  onCameraMove: (qwe) {
+                    controller.markercoords.value = qwe.target;
+                  },
+                  zoomControlsEnabled: false,
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(22.42259, -83.69854),
+                    zoom: 16,
+                  ),
+
+                  // markers: Set<Marker>.of(controller.markerlist.value),
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                ),
           const SearchBar(),
-          const Align(
-              alignment: Alignment.center, child: IconSelectMyLocation()),
+          Obx(() {
+            if (controller.isMarcador.value == true) {
+              return const Align(
+                  alignment: Alignment.center, child: IconSelectMyLocation());
+            } else {
+              return Container();
+            }
+          }),
+          Obx(() {
+            if (controller.isConfirmar.value == true) {
+              return const ConfirmarButton();
+            } else {
+              return Container();
+            }
+          }),
           const BottonModalShet(),
         ],
       ),
@@ -42,6 +74,56 @@ class MapaClientPage extends StatelessWidget {
 
       // BtnToggleUserRoute(),
       // BtnFollowUser(),
+    );
+  }
+}
+
+class ConfirmarButton extends StatelessWidget {
+  const ConfirmarButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MapaClientController controller = Get.find();
+
+    return Positioned(
+      bottom: Get.height * 0.12,
+      left: Get.width * 0.26,
+      child: MaterialButton(
+        color: const Color(0xFFf8ac00),
+        minWidth: 200,
+        onPressed: () async {
+          controller.isMarcador.value = false;
+
+          print(controller.position.value!.longitude);
+          print(controller.position.value!.latitude);
+
+          print(controller.markercoords.value!.latitude);
+          print(controller.markercoords.value!.longitude);
+
+          await controller.getRutas(
+              fromlat: controller.position.value!.latitude,
+              fromlong: controller.position.value!.longitude,
+              tolat: controller.markercoords.value!.latitude,
+              tolong: controller.markercoords.value!.longitude);
+
+          ProgressDialog progressDialog = ProgressDialog(context,
+              message: const Text("This is the message"),
+              title: const Text("This is the title"));
+
+          //You can set Message using this function
+          progressDialog.setTitle(const Text("Cargando"));
+          progressDialog
+              .setMessage(const Text("Calculando Ruta Por favor espere."));
+          progressDialog.show();
+
+          await Future.delayed(const Duration(seconds: 5));
+
+          progressDialog.dismiss();
+        },
+        child: const Text("Confirmar Ruta"),
+      ),
     );
   }
 }
--- lib/Cliente/pages/mapa/widgets/mapa_client_controller.dart
+++ lib/Cliente/pages/mapa/widgets/mapa_client_controller.dart
@@ -1,13 +1,156 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:marker_icon/marker_icon.dart';
+import 'package:http/http.dart' as http;
+import 'package:urbe_taxis/Cliente/models/rutas_mapbox.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 
 class MapaClientController extends GetxController {
   var markerlist = <Marker>[].obs;
   Rx<BitmapDescriptor?> icono = Rx<BitmapDescriptor?>(null);
   Rx<Position?> position = Rx<Position?>(null);
+  Rx<LatLng?> markercoords = Rx<LatLng?>(null);
   Rx<StreamSubscription<Position>?> positionStream =
       Rx<StreamSubscription<Position>?>(null);
+
+  var points = <LatLng>[].obs;
+
+  List<Polyline>? polyline = <Polyline>[].obs;
+
+  var isMarcador = false.obs;
+  var isConfirmar = false.obs;
+  var isRepintMapa = false.obs;
+
+  @override
+  void onInit() async {
+    icono.value = await convertIconFromAsset("assets/taxi.png");
+    await Geolocator.requestPermission();
+
+    position.value = await Geolocator.getCurrentPosition();
+    markercoords.value =
+        LatLng(position.value!.latitude, position.value!.longitude);
+
+    checkGps();
+    updateLocation();
+
+    polyline![0] = Polyline(
+      polylineId: const PolylineId('1'),
+      color: Colors.black,
+      width: 3,
+      points: points.value,
+    );
+
+    super.onInit();
+  }
+
+  Future getRutas(
+      {required double fromlat,
+      required double fromlong,
+      required double tolat,
+      required double tolong}) async {
+    var headers = {'token': ''};
+    var request = http.Request(
+        'GET',
+        Uri.parse(
+            'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/$fromlong,$fromlat;$tolong,$tolat.json?access_token=pk.eyJ1IjoiamF2aWVyOTgwNTA2MDIiLCJhIjoiY2wxcnV6cXdnMG8xNDNqbzk1cHBsNzRuciJ9.U_npzOoO9d4x4HYJ4uclCQ'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      final respuesta =
+          RutasMapbox.fromJson(await response.stream.bytesToString());
+
+      final polyline = decodePolyline(respuesta.routes![0].geometry!);
+
+      final latLongList = polyline
+          .map((point) => LatLng(point[0].toDouble(), point[1].toDouble()))
+          .toList();
+
+      points.value = latLongList;
+
+      isRepintMapa.value = true;
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
+  void addMarker(Marker marker) {
+    markerlist.add(marker);
+  }
+
+  addMarkerToMap(LatLng latLng, String id) {
+    markerlist.add(
+      Marker(
+        markerId: MarkerId(id),
+        position: latLng,
+        icon: icono.value!,
+        infoWindow:
+            const InfoWindow(title: "Taxi", snippet: "Chofe pare el Taxi"),
+        draggable: false,
+        zIndex: 2,
+        flat: true,
+        anchor: const Offset(0.5, 0.5),
+        rotation: position.value!.heading,
+      ),
+    );
+  }
+
+  void checkGps() async {
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isLocationServiceEnabled) {
+      bool locationGps = await Location.instance.requestService();
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  updateLocation() async {
+    try {
+      await _determinePosition();
+      position.value = await Geolocator.getLastKnownPosition();
+
+      positionStream.value = Geolocator.getPositionStream(
+        locationSettings: const LocationSettings(
+          distanceFilter: 1,
+        ),
+      ).listen((Position position) {
+        this.position.value = position;
+        addMarkerToMap(LatLng(position.latitude, position.longitude), "1");
+      });
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
+  Future<BitmapDescriptor> convertIconFromAsset(String imgPath) async {
+    var icon = await MarkerIcon.pictureAsset(
+        assetPath: imgPath, width: 80, height: 80);
+    return icon;
+  }
 }
